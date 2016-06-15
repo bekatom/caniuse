0d06b929d8019b18b4348b92791941473001652d
btomashvili <bekatomashvili@gmail.com>
2016-06-16 03:09:49 +0400

    udpate caniuse

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # caniuse.js. Detect new browser features if it's supported 
 
-Often we are looking new html5 or js features at (caniuse.js) http://caniuse.com/ to 
+Often we are looking new html5 or js features at [caniuse.js](http://caniuse.com/) to 
 determine if i can use this feature or not for my production code.
 
-caniuse.js -> It will detect everything if browser supports new HTML5 or JS features, 
-returns true || false . You can check may things, type it in your browser console 
+caniuse.js -> It will detect everything in browser side. It can tell us if I am able to 
+play mp3 music, use canvas with text api, or use web audio api. 
+supports or not new HTML5 or JS features, returns true || false. You can check may things, type it in your browser console 
 ```javascript
 caniuse.audio();
 caniuse.ogg();
--- caniuse.js
+++ caniuse.js
@@ -1,177 +1,243 @@
-(function(window){
+(function (window) {
 
-function CanIUse(){
+  function CanIUse() {
 
 
-    this._createElement = function(el){
+    this._createElement = function (el) {
       return document.createElement(el);
     };
 
-    this._createAudio = function(){
+    this._createAudio = function () {
       return document.createElement('audio');
     }
 
-    this._createInput = function(){
+    this._createInput = function () {
       return document.createElement('input');
     }
 
-    this.browserTest = function(){
-        console.log("browser test");
+    this.browserTest = function () {
+      console.log("browser test");
     };
 
-    this.audio = function(){
+    this.audio = function () {
       return !!this._createAudio().canPlayType;
     }
 
-    this.ogg = function(){
-        var ogg = this._createAudio();
-        return !!(ogg.canPlayType && ogg.canPlayType('audio/ogg; codecs="vorbis"').replace(/no/, ''));
+    this.ogg = function () {
+      var ogg = this._createAudio();
+      return !!(ogg.canPlayType && ogg.canPlayType('audio/ogg; codecs="vorbis"').replace(/no/, ''));
     };
 
-    this.mp3 = function(){
-        var mp3 =  this._createAudio();
-        return !!(mp3.canPlayType && mp3.canPlayType('audio/mpeg;').replace(/no/, ''));
+    this.mp3 = function () {
+      var mp3 = this._createAudio();
+      return !!(mp3.canPlayType && mp3.canPlayType('audio/mpeg;').replace(/no/, ''));
     };
 
-    this.wav = function(){
+    this.wav = function () {
       var wav = this._createAudio();
       return !!(wav.canPlayType && wav.canPlayType('audio/wav; codecs="1"').replace(/no/, ''));
     };
 
-    this.acc = function(){
+    this.acc = function () {
       var a = this._createAudio()
       return !!(a.canPlayType && a.canPlayType('audio/mp4; codecs="mp4a.40.2"').replace(/no/, ''));
     };
 
-    this.canvas = function(){
+    this.canvas = function () {
       return !!this._createElement('canvas').getContext;
     };
 
-    this.canvasTextApi  = function(){
+    this.canvasTextApi = function () {
       var c = this._createElement('canvas');
       return c.getContext && typeof c.getContext('2d').fillText == 'function';
     };
 
-    this.command = function(){
+    this.command = function () {
       return 'type' in this._createElement('command')
     };
 
-    this.datalist = function(){
+    this.datalist = function () {
       return 'options' in this._createElement('datalist')
     };
 
-    this.datails = function(){
+    this.datails = function () {
       return 'open' in this._createElement('details');
     };
 
-    this.formConstraintValidation = function(){
+    this.formConstraintValidation = function () {
       return 'noValidate' in this._createElement('form')
     };
 
-    this.iframeSandBox = function(){
+    this.iframeSandBox = function () {
       return 'sandbox' in this._createElement('iframe')
     };
 
-    this.iframeSrcDoc = function(){
+    this.iframeSrcDoc = function () {
       return 'srcdoc' in this._createElement('iframe')
     };
 
-    this.inputAutoFocus = function(){
+    this.inputAutoFocus = function () {
       return 'autofocus' in this._createInput();
     };
 
-    this.inputPlaceholder = function(){
+    this.inputPlaceholder = function () {
       return 'placeholder' in this._createInput();
     };
 
-    this.textareaPlaceholder = function(){
+    this.textareaPlaceholder = function () {
       return 'placeholder' in this._createElement('textarea')
     };
 
-    this.inputTypeColor = function(){
+    this.inputTypeColor = function () {
       var i = this._createInput();
       i.setAttribute('type', 'color');
       return i.type !== 'text';
     };
 
-    this.inputTypeEmail = function(){
+    this.inputTypeEmail = function () {
       var i = this._createInput();
       i.setAttribute('type', 'email');
       return i.type !== 'text';
     };
 
-    this.inputTypeNumber = function(){
+    this.inputTypeNumber = function () {
       var i = this._createInput();
       i.setAttribute('type', 'number');
       return i.type !== 'text';
     };
 
-    this.inputTypeRange = function(){
+    this.inputTypeRange = function () {
       var i = this._createInput();
       i.setAttribute('type', 'range');
       return i.type !== 'text';
     };
 
-    this.inputTypeSearch = function(){
+    this.inputTypeSearch = function () {
       var i = this._createInput();
       i.setAttribute('type', 'search');
       return i.type !== 'text';
     };
 
-    this.inputTypeTel = function(){
+    this.inputTypeTel = function () {
       var i = this._createInput();
       i.setAttribute('type', 'tel');
       return i.type !== 'text';
     };
 
-    this.inputTypeUrl = function(){
+    this.inputTypeUrl = function () {
       var i = this._createInput();
       i.setAttribute('type', 'url');
       return i.type !== 'text';
     };
 
-    this.inputTypeDate = function(){
+    this.inputTypeDate = function () {
       var i = this._createInput();
       i.setAttribute('type', 'date');
       return i.type !== 'text';
     };
 
-    this.inputTypeTime = function(){
+    this.inputTypeTime = function () {
       var i = this._createInput();
       i.setAttribute('type', 'time');
       return i.type !== 'text';
     };
 
-    this.inputTypeDateTime = function(){
+    this.inputTypeDateTime = function () {
       var i = this._createInput();
       i.setAttribute('type', 'datetime');
       return i.type !== 'text';
     };
 
 
-    this.inputDateTimeLocal = function(){
+    this.inputDateTimeLocal = function () {
       var i = this._createInput();
       i.setAttribute('type', 'datetime-local');
       return i.type !== 'text';
     };
 
-    this.inputTypeMonth = function(){
-      var i  = this._createElement();
+    this.inputTypeMonth = function () {
+      var i = this._createElement();
       i.setAttribute('type', 'month');
       return i.type !== 'text';
     };
 
-    this.inputTypeWeek = function(){
+    this.inputTypeWeek = function () {
       var i = this._createElement();
       i.setAttribute('type', 'week');
       return i.type !== 'text';
     };
 
+    ///
+    this.crossDocumentMessaging = function () {
+      return !!window.postMessage;
+    };
 
-}
+    this.dragAndDrop = function () {
+      return 'draggable' in this._createElement('span')
+    };
 
-var caniuse =  new CanIUse();
-window.caniuse = caniuse;
+    this.fileApi = function () {
+      return typeof FileReader != 'undefined';
+    };
+
+    this.geolocation = function () {
+      return !!navigator.geolocation;
+    };
+
+    this.history = function () {
+      return !!(window.history && window.history.pushState);
+    };
+
+    this.localStorage = function () {
+      try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+      } catch (e) {
+        return false;
+      }
+    };
+
+    this.microdata = function () {
+      return !!document.getItems;
+    };
+
+    this.offlineWebApplication = function () {
+      return !!window.applicationCache;
+    };
+
+    this.serverSentEvents = function () {
+      return typeof EventSource !== 'undefined';
+    };
+
+    this.sessionStorage = function () {
+      try {
+        return 'sessionStorage' in window && window['sessionStorage'] !== null;
+      } catch (e) {
+        return false;
+      }
+    };
+
+    this.svg = function () {
+      return !!(document.createElementNS && document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect);
+    };
+
+    this.svgInTextHtml = function () {
+      var e = document.createElement('div');
+      e.innerHTML = '<svg></svg>';
+      return !!(window.SVGSVGElement && e.firstChild instanceof window.SVGSVGElement);
+    };
+
+    this.undo = function(){
+      return typeof UndoManager !== 'undefined';
+    };
+
+    this.indexDB = function(){
+      return !!window.indexedDB;
+    };
+
+  }
+
+  var caniuse = new CanIUse();
+  window.caniuse = caniuse;
 
 
 })(window);
